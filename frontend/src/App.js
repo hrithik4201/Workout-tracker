6d5786efd14d7f438ee735f4b4e523b2ce5ac718
hrithik4201 <hrithikkumar2000@gmail.com>
2023-06-29 00:09:08 +0530

    Removed all the warnings.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+
 import RoutesConfig from './routes';
-import SmallNav from './components/SmallNav';
 
 function App() {
-  const { user } = useAuthContext();
-
   return (
     <div className='App'>
       <BrowserRouter>
