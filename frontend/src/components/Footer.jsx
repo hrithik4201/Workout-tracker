cd46059af2cbc97f9f2b0fad8d41c10a0cd38d34
hrithikCliperact <hrithikkumar2000@gmail.com>
2023-11-18 19:27:33 +0530

    Removed logo from footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
-import Logo from "../assets/images/Logo-1.png";
+import { Box, Typography } from "@mui/material";
 
 const Footer = () => (
   <Box mt="75px">
-    {/* <Stack
-      gap='40px'
-      sx={{ alignItems: 'center' }}
-      flexWrap='wrap'
-      px='40px'
-      pt='24px'
-    >
-      <img src={Logo} alt='logo' style={{ width: '165px', height: '56px' }} />
-    </Stack> */}
     <Typography
       variant="h5"
       sx={{ fontSize: { lg: "28px", xs: "20px" } }}
